18864e9e8cfa30a931a1c447fc84662e68f9da6f
mannerhybrid <32623363+mannerhybrid@users.noreply.github.com>
2019-06-24 01:31:13 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
 sudo apt-get install python-matplotlib
 sudo apt-get install python3-tk
+pip3 install --upgrade Cython
+pip3 install --upgrade git+https://github.com/statsmodels/statsmodels
 pip3 install --user --requirement requirements.txt
 python3 run.py
